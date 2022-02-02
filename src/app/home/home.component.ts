46e3f770bf2b400afd5e2f70299940e21978be6d
adarracq <antoine.cheval.darracq@gmail.com>
2022-02-02 18:58:32 +0100

    online

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,12 +9,12 @@ export class HomeComponent implements OnInit {
   allImages = [];    
 
   pictures = [    
-    { "id": 1, "url": "assets/resources/img1.jpg", "url2": "assets/resources/img2.jpg" },    
-    { "id": 2, "url": "assets/resources/img3.jpg", "url2": "assets/resources/img4.jpg" },    
-    { "id": 3, "url": "assets/resources/img5.jpg", "url2": "assets/resources/img6.jpg" },
-    { "id": 4, "url": "assets/resources/img1.jpg", "url2": "assets/resources/img2.jpg" },    
-    { "id": 5, "url": "assets/resources/img3.jpg", "url2": "assets/resources/img4.jpg" },    
-    { "id": 6, "url": "assets/resources/img5.jpg", "url2": "assets/resources/img6.jpg" } 
+    { "id": 1, "url": "assets/img1.jpg", "url2": "assets/img2.jpg" },    
+    { "id": 2, "url": "assets/img3.jpg", "url2": "assets/img4.jpg" },    
+    { "id": 3, "url": "assets/img5.jpg", "url2": "assets/img6.jpg" },
+    { "id": 4, "url": "assets/img1.jpg", "url2": "assets/img2.jpg" },    
+    { "id": 5, "url": "assets/img3.jpg", "url2": "assets/img4.jpg" },    
+    { "id": 6, "url": "assets/img5.jpg", "url2": "assets/img6.jpg" } 
   ];     
 
 
